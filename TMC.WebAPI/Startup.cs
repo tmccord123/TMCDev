78ecf5de40d9b5843e3de8f61a4719447d347091
tmccord <localcord@gmail.com>
2015-08-21 15:21:05 +0530

    WEBAPI solution added

---
+++ TMC.WebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TMC.WebAPI.Startup))]
+namespace TMC.WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
