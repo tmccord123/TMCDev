2540a2a25a00c4bba366b25263294725a11f3bba
tmccord <localcord@gmail.com>
2015-08-27 11:31:38 +0530

    IlistingDTo added

---
+++ TMC.Shared/Functional/DataTransferObjects/IListingDTO.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+namespace TMC.Shared
+{
+    public interface IListingDTO : IDTO
+    {
+        /// <summary>
+        /// Gets or sets the Listing Id.
+        /// </summary>
+        int UserId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        int ListingId { get; set; }
+        string CompanyName { get; set; }
+        string ContactPerson { get; set; }
+        string City { get; set; }
+        int PinCode { get; set; }
+        string EmailId { get; set; }
+        string LandlineNo { get; set; }
+        string MobileNo { get; set; }
+        int YearStarted { get; set; } 
+       
+    }
+}
