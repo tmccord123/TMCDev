07b57f8c6e4c9559b463d9e8e04dd8b2f680aa7d
tmccord <localcord@gmail.com>
2015-08-31 08:51:26 +0530

    login page UI changes and Add edit Vendor Listing page changes

--- TMC.WebAPI/js/tmc.AddEditListing.js
+++ TMC.WebAPI/js/tmc.AddEditListing.js
@@ -9,6 +9,13 @@
             $(".act span").text(x);
             $(".prev span").text(y);
         });
+        
+
+/*        $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+        $('#myTabs a:first').tab('show') // Select first tab
+        $('#myTabs a:last').tab('show') // Select last tab
+        $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)*/
+
     });
     this.setDataFields = function () {
         
@@ -16,7 +23,10 @@
         
     };
 
+    this.showNextPrevTab = function (tabIndex) { 
+        $('.nav-tabs li:eq(' + tabIndex + ') a').tab('show'); // Select third tab (0-indexed)*/
+    };
 
- 
+
 }
 
