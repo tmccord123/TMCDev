d37604ce5c8458393177051a6153e06968faa547
tmccord <localcord@gmail.com>
2015-10-24 21:26:38 +0530

    GenericPrincipalExtensions.cs taken to a new class

--- TMC.WebAPI/Controllers/AccountController.cs
+++ TMC.WebAPI/Controllers/AccountController.cs
@@ -18,26 +18,7 @@ using TMC.Web.ViewModels;
 
 namespace TMC.Web.Controllers
 {
-    /// <summary>
-    /// This Extension is added for showing the logged in user's Name instead of user id 
-    /// </summary>
-    public static class GenericPrincipalExtensions
-    {
-        public static string FirstName(this IPrincipal user)
-        {
-            if (user.Identity.IsAuthenticated)
-            {
-                ClaimsIdentity claimsIdentity = user.Identity as ClaimsIdentity;
-                foreach (var claim in claimsIdentity.Claims)
-                {
-                    if (claim.Type == "FirstName")
-                        return claim.Value;
-                }
-                return "";
-            }
-            return "";
-        }
-    }
+
     [Authorize]
     public class AccountController : Controller
     {
---
+++ TMC.WebAPI/Helpers/GenericPrincipalExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace TMC.Web
+{
+    /// <summary>
+    /// This Extension is added for showing the logged in user's Name instead of user id 
+    /// </summary>
+    public static class GenericPrincipalExtensions
+    {
+        public static string FirstName(this IPrincipal user)
+        {
+            if (user.Identity.IsAuthenticated)
+            {
+                ClaimsIdentity claimsIdentity = user.Identity as ClaimsIdentity;
+                foreach (var claim in claimsIdentity.Claims)
+                {
+                    if (claim.Type == "FirstName")
+                        return claim.Value;
+                }
+                return "";
+            }
+            return "";
+        }
+    }
+}
