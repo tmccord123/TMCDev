8011f48a1845fd1a347ad26a101e12c58e616812
tmccord <tmccord@users.noreply.github.com>
2015-11-16 11:06:30 +0530

    testing merge 2

--- TMC.DTOModel/DataTransferObjects/CategoryDTO.cs
+++ TMC.DTOModel/DataTransferObjects/CategoryDTO.cs
@@ -9,7 +9,7 @@ namespace TMC.DTOModel
     /// <summary>
     /// Contract for Category DTO.
     /// </summary>
-    [DataContract(Name = "CategoryDTO", Namespace = "TMC.DTOModel")]
+    [DataContract(Name = "CategoryDTO", Namespace = "TMC.DTOModel3")]
     [EntityMapping("EntityDataModel.EntityModels.Category", MappingType.TotalExplicit)]
     [Serializable]
     [ViewModelMapping("TMC.Web.ViewModels.CategoryViewModel", MappingType.TotalExplicit)]
